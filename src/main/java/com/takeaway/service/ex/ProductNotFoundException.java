0d3c900ab4f0bcb6f9bdc43d556905aeb5b7ba00
susu <843454026@qq.com>
2022-10-26 14:49:07 +0800

    redisUpdate1

---
+++ src/main/java/com/takeaway/service/ex/ProductNotFoundException.java
@@ -0,0 +1,27 @@
+package com.takeaway.service.ex;
+
+/**
+ * @author kafka
+ * 缓存击穿解决异常
+ */
+public class ProductNotFoundException extends ServiceException{
+    public ProductNotFoundException() {
+        super();
+    }
+
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+
+    public ProductNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ProductNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ProductNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
